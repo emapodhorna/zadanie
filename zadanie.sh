13dcb4910c7c12357c87a61f227fdf5e246c6859
emapodhorna <ema.podhorna@gmail.com>
2022-11-20 19:01:03 +0100

    oprava

--- zadanie.sh
+++ zadanie.sh
@@ -4,9 +4,18 @@ PREM="https://www.bing.com/search?q="
 
 while [ "$#" != 0 ]
 do
-PREM="$PREM$1+"
-shift
+if [ "$#" != 1 ]
+then
+	PREM="$PREM$1+"
+	shift
+elif [ "$#" == 1 ]
+then
+	PREM="$PREM$1"
+	shift
+else
+	break
+fi
 done
 
-curl -s "$PREM" | tr -d '\n' | tr '<' '\n' | grep 'a href=' | sed 's/a href="//' | sed 's/".*//' | sort | uniq 
+curl -s "$PREM" | tr -d '\n' | tr '<' '\n' | grep 'href="https://' | sed 's/.*href="//' | sed 's/".*//' | sort | uniq | grep -Ev 'microsoft|bing|creativecommons' 
 
